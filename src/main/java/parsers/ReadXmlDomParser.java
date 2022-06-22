181e7ffa0c5a524a7ab97fa87efcb1ef74a9ca58
Sreecharan Shroff <charaan@gmail.com>
2022-06-22 22:31:45 +0530

    Create ReadXmlDomParser.java

---
+++ src/main/java/parsers/ReadXmlDomParser.java
@@ -0,0 +1,46 @@
+package parsers;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import javax.xml.XMLConstants;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+
+public class ReadXmlDomParser {
+    private static final String userDir = System.getProperty("user.dir");
+    private static final String FILENAME = userDir + "/results/cucumber.xml1";
+    private static String result = "ignore";
+
+    public static void main(String[] args) {
+        try {
+            if (new File(FILENAME).exists()) {
+                DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+                dbf.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+                DocumentBuilder db = dbf.newDocumentBuilder();
+                Document doc = db.parse(new File(FILENAME));
+                doc.getDocumentElement().normalize();
+                int failuresCount = Integer.parseInt(doc.getDocumentElement().getAttribute("failures"));
+                System.out.println("Total failures :" + failuresCount);
+                System.out.println("------");
+                if (failuresCount == 0) {
+                    result = "pass";
+                } else {
+                    result = "fail";
+                }
+            }
+            String path = userDir + "\\target\\functionalResult.txt";
+            if (new File(path).exists()) {
+                Files.delete(Paths.get(path));
+            }
+            Files.createFile(Paths.get(path));
+            Files.write(Paths.get(path), result.getBytes());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
